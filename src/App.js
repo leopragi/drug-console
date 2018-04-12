42ac462a632026badfc35a33643a0d13c85c35ab
leopragi <leopragi@gmail.com>
2018-04-12 20:28:57 +0530

    Add provider

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
-
-import {PrivateRouteWrapper, PublicRoute, Link, Router} from './components/RouteComponents';
-import CreateUser from './user/Create'
-import Login from './user/Login'
-import Dashboard from './user/Dashboard'
-import { Button } from 'antd';
+import {Provider} from 'react-redux';
 
 import store from './redux/store'
-import {userSignUpStart} from './redux/actions/actionCreators'
+import Router from './Router';
 
 class App extends Component {
 	render() {
-		var isLoggedIn = false;
-		let PrivateRoute = PrivateRouteWrapper(isLoggedIn);
 		return (
-			<Router>
-				<div>
-					<Button type="primary">Button</Button>
-					<Link to="/login">Login</Link>
-					<Link to="/user/create">Create User</Link>
-					<Link to="/dashboard">Dashboard</Link>
-					<PublicRoute path="/user/create" component={CreateUser}/>
-					<PrivateRoute redirectTo="/login" path="/dashboard" component={Dashboard}/>
-					<PrivateRoute path="/login" isLoggedIn={!isLoggedIn} component={Login}/>					
-				</div>
-			</Router>
+			<Provider store={store}>
+				<Router />
+			</Provider>
 		);
 	}
 }
 
-// store.dispatch(userSignUpStart())
 
 export default App;
