2ddbd6c5289a3c93141102a72e291cda4b1da720
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-12 20:51:29 +0530

    Add Card

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input } from 'antd';
+import { Input, Card as DefaultCard} from 'antd';
 
 export function TextInput(props){
     let {placeholder} = props;
@@ -7,3 +7,21 @@ export function TextInput(props){
                 placeholder={placeholder}
             />
 }
+
+
+export function Card(props){
+    let{title,extra, loading} = props;
+    return (
+        <DefaultCard 
+            style = {{width : 300}}
+            loading = {loading} 
+            title = {title}
+            extra = {extra} >
+            {props.children}
+        </DefaultCard>
+    );
+}
+
+
+
+    
