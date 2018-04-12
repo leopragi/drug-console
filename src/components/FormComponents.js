f4256603f6a985bffd7132cc97dc642e3ed3f00a
leopragi <leopragi@gmail.com>
2018-04-12 20:55:35 +0530

    Add TextInput

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { Input } from 'antd';
 
 export function TextInput(props){
-    let {placeholder} = props;
+    let {name, placeholder, size, value, type, onChange} = props;
     return <Input 
+                name={name}
+                size={size}
                 placeholder={placeholder}
+                type={type}
+                value={value}
+                onChange={onChange}
             />
 }
