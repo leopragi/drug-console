dc4c0c7f64b448d7db5b335bb08d7feed06c3fdf
leopragi <leopragi@gmail.com>
2018-04-12 17:46:33 +0530

    Add router components

---
+++ src/components/RouteComponents.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route as PublicRoute, Redirect, Link, BrowserRouter as Router} from "react-router-dom";
+
+const PrivateRoute = ({
+	redirectTo, // Redirect to this route if not logged in
+	component: Component,
+	isLoggedIn,
+	...rest
+	}) => {
+		return <PublicRoute
+		{...rest}
+		render={props => {
+			return isLoggedIn ?
+				<Component {...props} isLoggedIn/> : 
+				<Redirect
+					to={{
+						pathname: redirectTo
+						? redirectTo
+						: props.location.state ? props.location.state.from.pathname : "/",
+						state: { from: props.location }
+					}}
+				/>
+			}}
+		/>
+	};
+
+function PrivateRouteWrapper(isLoggedIn) {
+	return (props) => {
+		return PrivateRoute({isLoggedIn, ...props});
+	}
+}
+
+export  {PrivateRouteWrapper, PublicRoute, Link, Router};
