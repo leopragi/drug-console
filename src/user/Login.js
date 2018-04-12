269a85ec012109ab7fc2d2800f5f9fefcfbd4e72
leopragi <leopragi@gmail.com>
2018-04-12 20:55:55 +0530

    Consume TextInput and Add state

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import {TextInput} from '../components/FormComponents'
 
 class Login extends Component {
+
+    state = {
+        email : '',
+        password : ''
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        this.setState({[target.name] : target.value});
+    }
+
     render() {
-        console.log(this.props)
         return (
             <div>
-                Login
+                <TextInput 
+                    name="email"
+                    size="large"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleChange}                    
+                />
+                <TextInput 
+                    name="password"
+                    size="large"
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange}                    
+                />
             </div>
         )
     }
