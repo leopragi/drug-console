a44ab24295278ff7d9ab992129acec4c564e4031
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-12 22:35:57 +0530

    Add Container

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -1,40 +1,58 @@
 import React, { Component } from 'react';
 import { Input, Card as DefaultCard, Button as DefaultButton} from 'antd';
+import style from 'styled-components'
+
+const InputContainer = style.div`
+    padding : 0.3em
+    
+`;
+
+
 
 export function TextInput(props){
     let {name, placeholder, size, value, type, onChange} = props;
-    return <Input 
-                name={name}
-                size={size}
-                placeholder={placeholder}
-                type={type}
-                value={value}
-                onChange={onChange}
-            />
+    return (
+            <InputContainer>
+                <Input 
+                    name={name}
+                    size={size}
+                    placeholder={placeholder}
+                    type={type}
+                    value={value}
+                    onChange={onChange}
+                />
+            </InputContainer>
+            )
 }
 
 export function Button(props){
     let {name, size, loading} = props;
-    return <DefaultButton
-                name={name}
-                loading={loading} 
-                type="primary"
-                size={size}>
-                {props.children}
-            </DefaultButton>
+    return(
+            <InputContainer>
+                <DefaultButton
+                    name={name}
+                    loading={loading} 
+                    type="primary"
+                    size={size}>
+                    {props.children}
+                </DefaultButton>
+            </InputContainer>
+        )
 }
 
 
 export function Card(props){
     let {title,extra, loading} = props;
-    return (
-        <DefaultCard 
-            style = {{width : 300}}
-            loading = {loading} 
-            title = {title}
-            extra = {extra} >
-            {props.children}
-        </DefaultCard>
+        return (
+            <InputContainer>
+                <DefaultCard 
+                    style = {{width : 300}}
+                    loading = {loading} 
+                    title = {title}
+                    extra = {extra} >
+                    {props.children}
+                </DefaultCard>
+            </InputContainer>
     );
 }
 
