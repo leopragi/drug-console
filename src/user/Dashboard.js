2f0a9fe45aa04360abb64e7b4598ec658f22da7c
leopragi <leopragi@gmail.com>
2018-04-13 16:48:43 +0530

    Add dashboard

--- src/user/Dashboard.js
+++ src/user/Dashboard.js
@@ -1,12 +1,91 @@
 import React, { Component } from 'react'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import styled from 'styled-components';
+
+const DefaultHeader = Layout.Header;
+const DefaultContent = Layout.Content;
+const DefaultFooter = Layout.Footer; 
+const DefaultSider = Layout.Sider;
+const SubMenu = Menu.SubMenu;
+
+const Header = styled(DefaultHeader)`
+    background: #fff;
+    padding: 0;
+`;
+
+const Content = styled(DefaultContent)`
+    margin: 0 16px
+`;
+
+const Footer = styled(DefaultFooter)`
+    text-align: center
+`;
 
 class Dashboard extends Component {
+    state = {
+        collapsed: false,
+    };
+    
+    onCollapse = (collapsed) => {
+        this.setState({ collapsed });
+    }
+
     render() {
         return (
-            <div>
-                Dashboard
-            </div>
-        )
+            <Layout style={{ minHeight: '100vh'}}>
+                <DefaultSider 
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                >
+                <div className="logo" />
+                <Menu  theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1">
+                        <Icon type="pie-chart" />
+                        <span>Option 1</span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="desktop" />
+                        <span>Option 2</span>
+                    </Menu.Item>
+                    <SubMenu
+                        key="sub1"
+                        title={<span><Icon type="user" /><span>User</span></span>}
+                    >
+                        <Menu.Item key="3">Tom</Menu.Item>
+                        <Menu.Item key="4">Bill</Menu.Item>
+                        <Menu.Item key="5">Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="sub2"
+                        title={<span><Icon type="team" /><span>Team</span></span>}
+                    >
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9">
+                        <Icon type="file" />
+                        <span>File</span>
+                    </Menu.Item>
+                </Menu>
+            </DefaultSider>
+            <Layout>
+                <Header/>
+                <Content>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                    Bill is a cat.
+                    </div>
+                </Content>
+                <Footer>
+                    {/* Ant Design ©2016 Created by Nikhi and Leo */}
+                </Footer>
+            </Layout>
+        </Layout>
+        );
     }
 }
 
