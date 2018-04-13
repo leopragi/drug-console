644b05b02dbab2d99a9277a296b042ba377b6ace
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-13 10:07:28 +0530

    Add actions for login, signup,login check

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,15 +1,43 @@
-import {USER_SIGNUP_START, USER_SIGNUP_FINISH } from './actions'
+import {USER_SIGNUP_START, USER_SIGNUP_FINISH, USER_LOGIN_START, USER_LOGIN_FINISH, USER_CHECK_LOGIN_STATUS_START, USER_CHECK_LOGIN_STATUS_FINISH } from './actions'
 
-export function userSignUpStart(){
+export function userSignUpStart(credentials){
     return{
-        type: USER_SIGNUP_START
+        type: USER_SIGNUP_START,
+        payload : credentials
 
     }
 }
 
-export function userSignUpFinish(data){
+export function userSignUpFinish(user){
     return{
         type : USER_SIGNUP_FINISH,
-        payload : data
+        payload : user
+    }
+}
+
+export function userLoginStart(credentials){
+    return{
+        type : USER_LOGIN_START,
+        payload : credentials
+    }
+}
+
+export function userLoginFinish(user){
+    return{
+        type : USER_LOGIN_FINISH,
+        payload : user
+    }
+}
+
+export function userCheckLoginStatusStart(){
+    return{
+        type: USER_CHECK_LOGIN_STATUS_START
+    }
+}
+
+export function userCheckLoginStatusFinish(user){
+    return{
+        type: USER_CHECK_LOGIN_STATUS_FINISH ,
+        payload : user
     }
 }
