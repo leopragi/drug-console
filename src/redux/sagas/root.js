78b07f9624bd2f8c7e54a963109585a6c709dfc6
leopragi <leopragi@gmail.com>
2018-04-13 17:37:33 +0530

    Fix typo

--- src/redux/sagas/root.js
+++ src/redux/sagas/root.js
@@ -1,4 +1,4 @@
-import {USER_SIGNUP_START, USER_LOGIN_START, USER_CHECK_LOGIN_STATUS_START, USER_SIGN_OUT} from '../actions/actions'
+import {USER_SEND_VERIFICATION_MAIL_START, USER_SIGNUP_START, USER_LOGIN_START, USER_CHECK_LOGIN_STATUS_START, USER_SIGN_OUT} from '../actions/actions'
 import { takeEvery } from 'redux-saga/effects'
 
 import {userSignUpStart, userLoginStart,userSendVerificationMailStartRedux, userCheckLoginStatusStart, userSignOut} from './user'
@@ -8,7 +8,7 @@ function* rootSaga(){
     yield takeEvery(USER_CHECK_LOGIN_STATUS_START, userCheckLoginStatusStart)
     yield takeEvery(USER_LOGIN_START, userLoginStart)
     yield takeEvery(USER_SIGN_OUT, userSignOut)
-    yield takeEvery(USER_MAIL_VERIFICATION_CHECK_START, userSendVerificationMailStartRedux)
+    yield takeEvery(USER_SEND_VERIFICATION_MAIL_START, userSendVerificationMailStartRedux)
 }
 
 export default rootSaga;
