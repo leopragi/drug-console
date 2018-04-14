42a36a75547bd9182a31a492923568f6a3e8e38f
leopragi <leopragi@gmail.com>
2018-04-14 10:53:35 +0530

    Consume utils function

--- src/redux/sagas/admin.js
+++ src/redux/sagas/admin.js
@@ -2,22 +2,12 @@ import { put, call } from 'redux-saga/effects'
 import { auth, database } from '../../config/firebase'
 
 import {adminReadAllUserFinish } from '../actions/actionCreators'
-
-function readUsers(ref){
-    return new Promise((resolve, reject) => {
-        ref.on('value', (snapshot) => {
-            if(snapshot)
-                return resolve(snapshot.val());
-            else
-                return reject(snapshot.val())
-        })
-    }) 
-}
+import {firebaseReadFromRef} from '../../utils'
 
 export function* adminReadAllUserStart(){
     try{
         var users = database.ref('/users');
-        var users = yield call(readUsers, users);
+        var users = yield call(firebaseReadFromRef, users);
         yield put(adminReadAllUserFinish(users))
     }
     catch(e){   
