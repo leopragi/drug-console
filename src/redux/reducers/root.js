9f01c92c4b88a21e6bfe6d8aaa68085f3f75014f
leopragi <leopragi@gmail.com>
2018-04-14 10:13:23 +0530

    Consume reducer

--- src/redux/reducers/root.js
+++ src/redux/reducers/root.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import userReducer from './userReducer'
 import stringReducer from './stringReducer'
 import queryReducer from './queryReducer'
+import adminReducer from './adminReducer'
 
 const rootReducer = {
     string : stringReducer,
     user : userReducer,
-    queries : queryReducer
+    queries : queryReducer,
+    admin : adminReducer
 }
 
 export default combineReducers(rootReducer);
