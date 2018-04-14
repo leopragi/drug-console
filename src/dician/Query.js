0219226035a914f35b24d5e52e180184aedc516f
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-14 17:22:26 +0530

    Add Style to EditQuery

--- src/dician/Query.js
+++ src/dician/Query.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react'
 import Editor from 'tinymce-react';
+import {Card} from '../components/FormComponents'
+import {Tag, Icon} from 'antd'
+import style from 'styled-components'
+    
+const TagContainer = style.div`
+    display : flex;
+    align-items : center;
+    padding : 8px;
+`;
+
+const TitleContainer = style.div`
+    display : flex;
+    align-items : center;
+    font-size : 35px;
+    font-weight: bold;
+`;
+
+const EditorContainer = style.div``;
+const QueryContainer = style.div``;
 
 class Query extends Component {
 
@@ -11,21 +30,37 @@ class Query extends Component {
         }        
     }
 
+    
+
     handleEditorChange = (e) => {
         console.log('Content was updated:', e.target.getContent());
     }
 
     render() {
+        let {query} = this.props.location.state
         return(
-            <Editor 
-                apiKey="zuyjyxrgwf0wxyuv9mh90ew3aj0v6dpgg3uerlc2q9hrsaus"
-                initialValue={this.state.answer}
-                init={{
-                    plugins: 'link image code table',
-                    toolbar: 'undo redo | table | bold italic | alignleft aligncenter alignright | code'
-                }}
-                onChange={this.handleEditorChange}
-            />
+            <QueryContainer>  
+                <TitleContainer>
+                    {query.queries[0].query} 
+                </TitleContainer>
+               <TagContainer>
+                        <Tag color='#ffab00'> <Icon type="smile" /> {query.feedback}</Tag>
+                        <Tag color='#ffab00'>{'@'+query.at}</Tag>
+                        {query.answered ? <Tag color='#87d068'>resolved</Tag> : <Tag color='#f00'>pending</Tag>  }
+
+                </TagContainer>
+                <EditorContainer>
+                    <Editor
+                        apiKey="zuyjyxrgwf0wxyuv9mh90ew3aj0v6dpgg3uerlc2q9hrsaus"
+                        initialValue={this.state.answer}
+                        init={{
+                            plugins: 'link image code table',
+                            toolbar: 'undo redo | table | bold italic | alignleft aligncenter alignright | code'
+                        }}
+                        onChange={this.handleEditorChange}
+                    />
+                </EditorContainer>
+            </QueryContainer>
         )
     }
 }
