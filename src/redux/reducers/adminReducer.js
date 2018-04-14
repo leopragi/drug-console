96dc4f40ffd1875a8cea8ca9f8717382464a7e8b
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-14 10:55:41 +0530

    Add reducer for admin to read teams and stories

--- src/redux/reducers/adminReducer.js
+++ src/redux/reducers/adminReducer.js
@@ -1,13 +1,22 @@
-import {ADMIN_GET_ALL_USER_FINISH} from '../actions/actions'
+import {ADMIN_GET_ALL_USER_FINISH, ADMIN_READ_ALL_STORIES_FINISH, ADMIN_READ_ALL_TEAMS_FINISH} from '../actions/actions'
 
 const initialState = {
-    users : []
+    users : [],
+    stories :[],
+    teams : []
 };
 
 function reducer(state = initialState, action){
     switch(action.type){
         case ADMIN_GET_ALL_USER_FINISH:
             return { ...state, users : action.payload };
+
+        case ADMIN_READ_ALL_STORIES_FINISH:
+            return { ...state, stories : action.payload };
+
+        case ADMIN_READ_ALL_TEAMS_FINISH:
+            return { ...state, teams : action.payload };
+
     }
     return state;
 }
