ff8d0ac79e6b9d962295813053e6a48184193292
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-14 12:20:32 +0530

    Add list style

--- src/dician/Queries.js
+++ src/dician/Queries.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {List as DefaultList} from 'antd'
+import {List as DefaultList, Button, Spin} from 'antd'
 
 import {userReadQueriesStart} from '../redux/actions/actionCreators'
 
@@ -8,20 +8,47 @@ class Queries extends Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            loading: true,
+            loadingMore: false,
+            showLoadingMore: true,
+          }
         props.userReadQueriesStart(props.user.uid)
     }
 
+    
+   componentDidMount(){
+       this.setState({
+           loading : false,
+       })
+
+   }
+
+    onLoadMore = () => {
+        this.setState({
+          loadingMore: true,
+        });
+    }
 
     render() {
+        const { loading, loadingMore, showLoadingMore, data } = this.state;
         let {queries} = this.props;
+        const loadMore = showLoadingMore ? (
+            <div style={{ textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px' }}>
+              {loadingMore && <Spin />}
+              {!loadingMore && <Button onClick={this.onLoadMore}>Load More</Button>}
+            </div>
+          ) : null;
         return(
             <DefaultList
+                loadmore={loadMore}
                 itemLayout="horizontal"
                 dataSource={queries}
-                renderItem={query => (
+                
+                 renderItem={query => (
                     <DefaultList.Item>
                         <DefaultList.Item.Meta
-                            description = {query.queries[0].queryString}    
+                           description = {query.queries[0].queryString}    
                         />
                     </DefaultList.Item>
                 )}
