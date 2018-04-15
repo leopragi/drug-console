6a63427d88400998023e40bb38db04f6d23e40f8
leopragi <leopragi@gmail.com>
2018-04-15 18:34:05 +0530

    Add style

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import styled from 'styled-components'
 
 import {TextInput, Card, Button} from '../components/FormComponents'
 import {userLoginStart} from '../redux/actions/actionCreators'
+
+
+const FieldContainer = styled.div`
+`;
+
+const RecoverPasswordContainer = styled.div`
+    padding: 5px;
+`;
+
+
+const TitleContainer = styled.div`
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`;
+
+const LoginFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
 
 class Login extends Component {
 
     state = {
         email : '',
-        password : ''
+        password : '',
+        progress : false
     }
 
     handleChange = (event) => {
@@ -17,33 +40,48 @@ class Login extends Component {
     }
 
     handleLogin = (event) => {
+        this.setState({progress : true});
         this.props.userLoginStart(this.state)
     }
     
     render(){
         return(
             <Card>
-                <TextInput 
-                    name="email"
-                    size="large"
-                    type="email"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleChange}   
-                />
-                <TextInput 
-                    name="password"
-                    size="large"
-                    type="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}                    
-                />
-                <Button
-                    size="large"
-                    onClick={this.handleLogin}>
+                <TitleContainer>
                     Login
-                </Button>
+                </TitleContainer>
+                <FieldContainer>
+                    <TextInput 
+                        name="email"
+                        size="large"
+                        type="email"
+                        placeholder="Email"
+                        disabled={this.state.progress}
+                        value={this.state.email}
+                        onChange={this.handleChange}   
+                    />
+                    <TextInput 
+                        name="password"
+                        size="large"
+                        type="password"
+                        placeholder="Password"
+                        disabled={this.state.progress}
+                        value={this.state.password}
+                        onChange={this.handleChange}                    
+                    />
+                </FieldContainer>
+                <LoginFooter>
+                    <RecoverPasswordContainer>
+                        Forgot password?
+                    </RecoverPasswordContainer>
+                    <Button
+                        loading={this.state.progress}
+                        type="primary"
+                        size="large"
+                        onClick={this.handleLogin}>
+                        Login
+                    </Button>
+                </LoginFooter>
             </Card>
         )
     }
