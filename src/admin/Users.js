0e417ae958732cb196f7582316178abc1ed52244
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-04-15 09:48:29 +0530

    Add Tabs for Users Dashboard Admin

--- src/admin/Users.js
+++ src/admin/Users.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import style from 'styled-components'
-import {List as DefaultList} from 'antd'
+import {List as DefaultList, Tabs,} from 'antd'
 
-import { adminReadAllUserStart } from '../redux/actions/actionCreators'
+import { adminReadAllUserStart, adminReadUnauthorizedUsersStart, adminReadAuthorizedUsersStart } from '../redux/actions/actionCreators'
 
 class Users extends Component {
   
@@ -19,18 +19,28 @@ class Users extends Component {
 
     render() {
         let {users} = this.props;
+        console.log(users)
         return(
-            <DefaultList
-                itemLayout="horizontal"
-                dataSource={users}
-                renderItem={user => (
-                    <DefaultList.Item>
-                        <DefaultList.Item.Meta
-                            description = {user.email}    
-                        />
-                    </DefaultList.Item>
-                )}
-            />
+            <Tabs defaultActiveKey="2">
+            <Tabs.TabPane tab={<span>UNAUTHORIZED USERS</span>} key="1">
+              
+            </Tabs.TabPane>
+            <Tabs.TabPane tab={<span>AUTHORIZED USERS</span>} key="2">
+
+            </Tabs.TabPane>
+          </Tabs>
+            
+            // <DefaultList
+            //     itemLayout="horizontal"
+            //     dataSource={users}
+            //     renderItem={user => (
+            //         <DefaultList.Item>
+            //             <DefaultList.Item.Meta
+            //                 description = {user.email}    
+            //             />
+            //         </DefaultList.Item>
+            //     )}
+            // />
         )
     }
 }
@@ -42,4 +52,4 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 
-export default connect(mapStateToProps,{adminReadAllUserStart})(Users);
+export default connect(mapStateToProps,{adminReadAllUserStart, adminReadUnauthorizedUsersStart, adminReadAuthorizedUsersStart})(Users);
