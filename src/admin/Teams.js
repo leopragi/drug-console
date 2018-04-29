ed7296fef2dc95e7b6aedafa77a7f5c3391b183d
leopragi <leopragi@gmail.com>
2018-04-29 20:22:28 +0530

    Add team create card

--- src/admin/Teams.js
+++ src/admin/Teams.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import {Card} from 'antd'
-
+import {Card, Row, Col, Form, Button} from 'antd'
+import {Input} from '../components/FormComponents'
 
 class Teams extends Component {
     render() {
+
+        const domainId = "ooty";
         return (
-            <div>
-                <Card title="Create Team"
-                    style={{ width: 300 }}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </div>
+            <Row>
+                <Col span={6}>
+                    <Card title="Create Team"
+                        style={{ width: 300 }}>
+                        <Form>
+                        <Input placeholder="Team name"/> 
+                            <Input addonBefore={`${domainId}.`} addonAfter=".team" placeholder="Team Id"/> 
+                            <Button type="primary" htmlType="submit">
+                                Create
+                            </Button>
+                        </Form>
+                   </Card>
+                </Col>
+                <Col span={18}>
+                </Col>
+            </Row>
         )
     }
 }
