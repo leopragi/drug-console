72490c4dd9861f32978fa65f77fbff2f826298cf
leopragi <leopragi@gmail.com>
2018-04-29 19:45:59 +0530

    Add teams component

---
+++ src/admin/Teams.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Card} from 'antd'
+
+
+class Teams extends Component {
+    render() {
+        return (
+            <div>
+                <Card title="Create Team"
+                    style={{ width: 300 }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default Teams;
